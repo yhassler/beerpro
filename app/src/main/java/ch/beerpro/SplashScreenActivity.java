94ecc6cb66771d0e28fed887da8c1d5a281358ab
Mirko Stocker <me@misto.ch>
2018-07-30 16:42:53 +0200

    migrate to androidx

--- app/src/main/java/ch/beerpro/SplashScreenActivity.java
+++ app/src/main/java/ch/beerpro/SplashScreenActivity.java
@@ -1,6 +1,6 @@
 package ch.beerpro;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
